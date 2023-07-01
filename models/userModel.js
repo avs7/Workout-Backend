06f4aabc9316caf3a6b047124a171b5c0dca4cb4
Aleksandr <aslyusarev@gmail.com>
2023-07-01 11:55:26 -0700

    removed arrow function since using .this, added signupUser functionality

--- models/userModel.js
+++ models/userModel.js
@@ -16,7 +16,7 @@ const userSchema = new Schema({
 })
 
 // static signup method for signup controller
-userSchema.statics.signup = async (email, password) => {
+userSchema.statics.signup = async function (email, password) {
   const exists = await this.findOne({ email })
 
   if (exists) {
