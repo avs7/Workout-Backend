5fafe69c21906a2cffdf5c2081c0a76db48ea8b5
Aleksandr <aslyusarev@gmail.com>
2023-07-01 11:05:49 -0700

    set up user routes, controller and db schema

--- server.js
+++ server.js
@@ -3,16 +3,19 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
+
 const app = express()
 
 app.use(express.json())
 
-// app.use((req, res, next) => {
-//   console.log(req, path, req.method)
-//   next()
-// })
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
 
 app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
 
 //connect to db
 mongoose
